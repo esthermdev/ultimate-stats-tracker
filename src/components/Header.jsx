843e046e88661dea7f8d7d5372c54c996eb2a748
Esther Devadas <esmd258@gmail.com>
2025-04-23 14:49:09 +0800

    fix: updated header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ onExport }) {
   return (
     <header className="app-header">
-      <h1>Ultimate Frisbee Stats</h1>
+      <h1>Ultimate Frisbee Stats Tracker</h1>
       <div className="header-actions">
         <button onClick={onExport} className="export-button">
           Export to Excel
